b80c2392a3fa488ae6e84a53a325a236e72c48e9
msheldon32 <mattsheldon914@gmail.com>
2015-09-27 01:15:40 -0400

    broken build

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var roomates = [], tasks = [], charges = [];
+var roomates = [], tasks = [], charges = [], chores = [];
 
 app.get('/', function (req, res) {
 });
@@ -20,6 +20,29 @@ app.post('/roomates', function (req, res) {
     res.send("added");
 });
 
+app.delete('/roomates', function (req, res) {
+	res.send(roomates[0]);
+	var index = 0;
+	var found = false;
+	for (; index < roomates.length; index++)
+	{
+		if (roomates[index].ID === req.body.ID)
+		{
+			break;
+			found = true;
+		}
+	}
+	if (found)
+	{
+		res.send ("removed entry");
+		roomates.splice(index, 1);
+	}
+	else
+	{
+		res.send ("no matching entry");
+	}
+});
+
 //tasks//
 app.get('/tasks', function(req, res) {
     res.send(tasks);
@@ -28,6 +51,19 @@ app.get('/tasks', function(req, res) {
 app.post('/tasks', function (req, res) {
     tasks.push(req.body);
     res.send("added");
+});
+
+app.delete('/tasks', function (req, res) {
+	index = tasks.indexOf (req.body);
+	if (index != -1)
+	{
+		res.send ("removed entry");
+		tasks.splice(index, 1);
+	}
+	else
+	{
+		res.send ("no matching entry");
+	}
 });
 
 //charges//
@@ -40,12 +76,41 @@ app.post('/charges', function (req, res) {
     res.send("added");
 });
 
-app.use("/", function (req, res) {
-  res.send("invalid path");
+app.delete('/charges', function (req, res) {
+	index = charges.indexOf (req.body);
+	if (index != -1)
+	{
+		res.send ("removed entry");
+		charges.splice(index, 1);
+	}
+	else
+	{
+		res.send ("no matching entry");
+	}
 });
 
+//chores//
+app.get('/chores', function(req, res) {
+	res.send(chores);
+});
 
+app.post('/chores', function(req, res) {
+	chores.push(req.body);
+	res.send("added");
+});
 
+app.delete('/chores', function (req, res) {
+	index = chores.indexOf (req.body);
+	if (index != -1)
+	{
+		res.send ("removed entry");
+		chores.splice(index, 1);
+	}
+	else
+	{
+		res.send ("no matching entry");
+	}
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
