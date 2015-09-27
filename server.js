9770a040e6312b279ed6dac078b45afc2e7f1699
Brian Murphy <brainmurphy1@gmail.com>
2015-09-27 04:34:57 -0400

    changed port

--- server.js
+++ server.js
@@ -128,10 +128,9 @@ app.delete('/chores', function (req, res) {
 		res.send ("no matching entry");
 	}
 });
-
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 1337;
+var server = app.listen(port, function () {
   var host = server.address().address;
-  var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
